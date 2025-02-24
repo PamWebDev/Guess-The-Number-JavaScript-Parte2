f220612dadf69e9b16cd431cc3a5bed657f6b372
unknown <pampambriceno.dev@gmail.com>
2025-02-24 16:41:09 -0500

    feat: Crear funcion para generar el numero secreto

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+let numeroSecreto = generarNumeroSecreto();
+
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
 
 function intentoDeUsuario(){
     alert('click desde el boton')
+}
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
 }
 
 asignarTextoElemento('h1', 'Juego del número secreto')
