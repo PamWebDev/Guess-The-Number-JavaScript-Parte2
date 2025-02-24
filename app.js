09d1ba15181153e815373da42ff941e37b859980
unknown <pampambriceno.dev@gmail.com>
2025-02-24 16:24:29 -0500

    feat: Agregar título e indicaciones de juego

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del número secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un número entre 1 y 10';
+
+function intentoDeUsuario(){
+    alert('click desde el boton')
+}
